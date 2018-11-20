1276fb6da4f11ec38b0d18a24877f0584da8d628
douglasBaumgratz <douglas.baumgratz@gmail.com>
2018-11-20 19:28:24 -0200

    Agrupando DbHelper

--- app/src/main/java/br/ufjf/dcc196/trabalho1_ramon_douglas/DbHelper.java
+++ app/src/main/java/br/ufjf/dcc196/trabalho1_ramon_douglas/DbHelper.java
@@ -4,21 +4,24 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class EventoDbHelper extends SQLiteOpenHelper {
+public class DbHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Biblioteca.db";
 
-    public EventoDbHelper(Context context) {
+    public DbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(EventoContract.CREATE_EVENTO);
+        sqLiteDatabase.execSQL(ParticipanteContract.CREATE_PARTICIPANTE);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(ParticipanteContract.DROP_PARTICIPANTE);
         sqLiteDatabase.execSQL(EventoContract.DROP_EVENTO);
         onCreate(sqLiteDatabase);
     }
